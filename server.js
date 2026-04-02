9906cbb7ad88b47332c32614761ae2a919e03ff9
RyanPonnievNC <ryanponnievcn@email.com>
2026-04-02 00:27:34 -0300

    -Foi efetuada uma alteração que as pessoas podem acessar meu site

--- server.js
+++ server.js
@@ -6,3 +6,13 @@ app.use(express.static('public'));
 app.listen(3000, () => {
   console.log('Servidor rodando em http://localhost:3000');
 });
+
+const express = require('express');
+const path = require('path');
+
+// servir arquivos da pasta public
+app.use(express.static('public'));
+
+app.listen(3000, () => {
+  console.log('Servidor rodando');
+});
